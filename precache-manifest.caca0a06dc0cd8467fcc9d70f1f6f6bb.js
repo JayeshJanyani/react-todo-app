253ac93294d00135c5973cea6736073c9b743d79
Jayesh Janyani <jjanyani@oradev.oraclecorp.com>
2020-01-25 16:41:02 +0530

    Updates

---
+++ precache-manifest.caca0a06dc0cd8467fcc9d70f1f6f6bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9a7609091470e1c08c77097bc5f352",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "3edb0fcd0ee78bdd784e",
+    "url": "/react-todo-app/static/css/main.abe6d670.chunk.css"
+  },
+  {
+    "revision": "cb394e867ab855a90fe4",
+    "url": "/react-todo-app/static/js/2.7aa98d37.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo-app/static/js/2.7aa98d37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3edb0fcd0ee78bdd784e",
+    "url": "/react-todo-app/static/js/main.abde670c.chunk.js"
+  },
+  {
+    "revision": "50e34dadb3b1b4d10e31",
+    "url": "/react-todo-app/static/js/runtime-main.cac1d3e0.js"
+  }
+]);
